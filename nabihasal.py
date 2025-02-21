b5f13532c70d5e9f23046a5047a9cfbd89265078
Hisham Srour <hishamsrour8@gmail.com>
2025-02-21 16:33:45 +0200

    added percentages

--- nabihasal.py
+++ nabihasal.py
@@ -1,3 +1,6 @@
 def main():
     salary = float(input("Enter your salary for the month: $"))
     month = input("Enter the name of the month: ")
+    savings_percentage = float(input("Enter the percentage of your salary you want to save: "))
+    rent_percentage = float(input("Enter the percentage of your salary for rent: "))
+    electricity_percentage = float(input("Enter the percentage of your salary for electricity: "))
