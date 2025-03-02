4c7f13f48dd94e7938d4b416b26a020db3e6a79a
Hisham Srour <hishamsrour8@gmail.com>
2025-03-02 20:03:47 +0200

    Created class bike

--- rent.py
+++ rent.py
@@ -16,7 +16,7 @@ class Vehicle:
     
     def set_rental_price_per_day(self, new_price):
         self.__rental_price_per_day = new_price
-        
+
 class Car(Vehicle):
     def __init__(self, brand, model, year, rental_price_per_day, seating_capacity):
         super().__init__(brand, model, year, rental_price_per_day)
@@ -24,4 +24,16 @@ class Car(Vehicle):
     
     def display_info(self):
         print(f"Car: {self.brand} {self.model}, Year: {self.year}, Seats: {self.seating_capacity}, Rental Price: ${self.get_rental_price_per_day()}/day")
+class Bike(Vehicle):
+    def __init__(self, brand, model, year, rental_price_per_day, engine_capacity):
+        super().__init__(brand, model, year, rental_price_per_day)
+        self.engine_capacity = engine_capacity
+    
+    def display_info(self):
+        print(f"Bike: {self.brand} {self.model}, Year: {self.year}, Engine: {self.engine_capacity}cc, Rental Price: ${self.get_rental_price_per_day()}/day")
+
+
+def show_vehicle_info(vehicle):
+    vehicle.display_info()
+
 
